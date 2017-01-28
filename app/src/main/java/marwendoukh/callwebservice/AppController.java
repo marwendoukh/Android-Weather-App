2784453c25bd3787b950e8d4024084fb677efffd
Marwen Doukh <marwen.doukh@gmail.com>
2017-01-28 22:58:54 +0100

    add missing files

---
+++ app/src/main/java/marwendoukh/callwebservice/AppController.java
@@ -0,0 +1,43 @@
+package marwendoukh.callwebservice;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class AppController {
+
+    private static AppController mInstance;
+    private static Context mCtx;
+    private RequestQueue mRequestQueue;
+
+    private AppController(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized AppController getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new AppController(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(@NonNull final Request<T> request) {
+        getRequestQueue().add(request);
+    }
+
+    public <T> void addToRequestQueueWithTag(@NonNull final Request<T> request, String tag) {
+        request.setTag(tag);
+        getRequestQueue().add(request);
+    }
+}
